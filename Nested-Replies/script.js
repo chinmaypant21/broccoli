62bb8299733323b1c236aa7ac288e1e1e88fc20a
chinmaypant21 <chinmay@growagro.in>
2024-04-15 17:39:01 +0530

    updated

--- Nested-Replies/script.js
+++ Nested-Replies/script.js
@@ -28,26 +28,26 @@ let repliesData = [
 
 const container = document.getElementById('container');
 
-function recursiveDataAdd(replies, parentElement){
-    replies.forEach(reply => {
-        const element = document.createElement('div')
-        element.style.paddingLeft = `${12}px`
-        element.classList.add('reply-container')
-        element.appendChild(createReplyBody(reply.msg, element))
+function addReply(reply) {
+    const element = document.createElement('div')
+    element.style.paddingLeft = `${12}px`
+    element.classList.add('reply-container')
 
-        parentElement.appendChild(element)
+    const replyBody = createReplyBody(reply.msg, element)
+    element.appendChild(replyBody)
 
-        if(reply.replies && reply.replies.length) {
-            recursiveDataAdd(reply.replies, element)
-        }
-    })
+    return element
+
 }
 
 function createReplyBody(text, parentElement){
     const element = document.createElement('div')
     element.classList.add('reply-body')
-    element.appendChild(createMessageElement(text))
-    element.appendChild(createReplyElement(parentElement))
+
+    const messageElement  = createMessageElement(text)
+    const replyButton     = createReplyButton(parentElement)
+    element.appendChild(messageElement)
+    element.appendChild(replyButton)
 
     return element;
 
@@ -60,19 +60,27 @@ function createMessageElement(text) {
     return element
 }
 
-function createReplyElement(parentElement) {
-    console.log(parentElement)
+function createReplyButton(parentElement) {
     const button = document.createElement('button')
     button.textContent = 'Reply'
     button.className = 'reply-btn'
     button.addEventListener('click', () => {
-        recursiveDataAdd([
-            {msg: 'ADDED'},
-        ],
-        parentElement
-    )
+        const replyElement = addReply({msg: 'ADDED'})
+        parentElement.appendChild(replyElement)
     })
     return button
+}
+
+
+function recursiveDataAdd(replies, parentElement){
+    replies.forEach(reply => {
+       const replyElement = addReply(reply)
+        parentElement.appendChild(replyElement)
+
+        if(reply.replies && reply.replies.length) {
+            recursiveDataAdd(reply.replies, replyElement)
+        }
+    })
 }
 
 recursiveDataAdd(repliesData, container)
